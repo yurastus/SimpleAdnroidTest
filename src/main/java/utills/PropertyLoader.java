c309fb48c0cc2cb292c7db064967a52ab9774c62
Yurii_Stus <stusyura@gmail.com>
2018-02-19 13:24:56 +0200

    optimized properties reading

--- src/main/java/utills/PropertyLoader.java
+++ src/main/java/utills/PropertyLoader.java
@@ -8,17 +8,18 @@ public class PropertyLoader {
     private static final String PROP_FILE = "/android.properties";
     private static Properties props = new Properties();
 
+    static {
+        try {
+            props.load(PropertyLoader.class.getResourceAsStream(PROP_FILE));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private PropertyLoader(){}
 
     public static String loadProperty(String name) {
-
-        try {
-            props.load(PropertyLoader.class.getResourceAsStream(PROP_FILE));
-            return props.getProperty(name);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return props.getProperty(name);
     }
 
 }

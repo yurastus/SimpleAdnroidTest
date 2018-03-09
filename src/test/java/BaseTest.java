d4ff1c2d13adaf062f07590d0cfc0e4ee413dee0
Yurii_Stus <stusyura@gmail.com>
2018-03-09 15:04:17 +0200

    added automatic start for Appium server from java code

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,3 +1,4 @@
+import io.appium.java_client.service.local.AppiumDriverLocalService;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -6,15 +7,21 @@ import utills.Driver;
 public class BaseTest {
 
     protected WebDriver driver;
+    private AppiumDriverLocalService service;
 
     @BeforeTest
     public void setUp() {
+        service = AppiumDriverLocalService.buildDefaultService();
+        service.start();
+
         driver = new Driver().initDriver();
     }
 
     @AfterTest
     public void tearDown() {
         driver.quit();
+
+        service.stop();
     }
 
 }
